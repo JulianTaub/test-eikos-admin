94ff91890684662cecb7eb09deee4c2c8a69893f
JulianTaub <jtaub9503@gmail.com>
2019-09-12 00:37:10 -0400

    Creates test react admin app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} edit={EditGuesser}/>
+        <Resource name="posts" list={ListGuesser} edit={EditGuesser}/>
+        <Resource name="comments" list={ListGuesser} edit={EditGuesser}/>
+    </Admin>
+);
 
 export default App;
